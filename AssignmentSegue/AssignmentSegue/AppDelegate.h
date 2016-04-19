6ac3f0e118eff8003176d1c8f81f47cfde6a6f64
Devashish Badlani <deebee2307@gmail.com>
2016-04-19 14:03:33 -0700

    Commit App
    Commit App

---
+++ AssignmentSegue/AssignmentSegue/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AssignmentSegue
+//
+//  Created by Devashish Badlani on 4/15/16.
+//  Copyright © 2016 Devashish Badlani. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
