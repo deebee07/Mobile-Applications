b01ce55576d23b718b0892c5a3c8cda406aae757
Devashish Badlani <deebee2307@gmail.com>
2016-04-19 14:04:24 -0700

    Commit App
    Commit App

---
+++ AssignmentSegue/AssignmentSegue/CloseButton.m
@@ -0,0 +1,19 @@
+//
+//  CloseButton.m
+//  AssignmentSegue
+//
+//  Created by Devashish Badlani on 4/15/16.
+//  Copyright © 2016 Devashish Badlani. All rights reserved.
+//
+
+#import "CloseButton.h"
+
+@implementation CloseButton
+
+
+- (void) perform {
+    
+    exit(0);
+}
+
+@end
