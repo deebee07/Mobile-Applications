ea7fc131713f1372396081905477db5020c51c11
Devashish Badlani <deebee2307@gmail.com>
2016-05-12 07:41:43 -0700

    Attendance Display
    Attendance Display

---
+++ Attendance_Display/Attendance_Display/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Attendance_Display
+//
+//  Created by Devashish Badlani on 5/8/16.
+//  Copyright © 2016 Devashish Badlani. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
